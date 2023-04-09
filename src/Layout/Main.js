681dce650d546a25a438c4e70b33a2cccca5c08a
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-04-09 20:22:00 +0600

    first commit

---
+++ src/Layout/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h4>This is main component.</h4>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Routes/Routes/Router.js
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+    }
+])
