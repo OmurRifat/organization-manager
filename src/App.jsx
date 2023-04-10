d86552c41cec5ea51aed2556abdb87301aa78e3b
unknown <mdemonhossain9527@gmail.com>
2023-04-10 16:28:09 +0600

    fix-nav-hero-section-isshu

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes/Router'
-import MainLayout from './Layout/MainLayout';
-import Home from './Pages/Home/Home/Home';
+
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      children: [
-        {
-          path: "/",
-          element: <Home></Home>,
-        },
-      ],
-    },
-   
-  ]);
+  
   return (
-    <div className="mx-auto">
+    <div className="max-w-[1440px] mx-auto">
       <RouterProvider router={ router }></RouterProvider>
     </div>
   )
