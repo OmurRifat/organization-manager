c4769b3350bfffca75a79e543ae91cb921d684ab
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-04-10 15:45:09 +0600

    Some changes added in about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import router from './Routes/Routes/Router'
-import MainLayout from './Layout/MainLayout';
-import Home from './Pages/Home/Home/Home';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      children: [
-        {
-          path: "/",
-          element: <Home></Home>,
-        },
-      ],
-    },
-   
-  ]);
   return (
     <div className="mx-auto">
       <RouterProvider router={ router }></RouterProvider>
