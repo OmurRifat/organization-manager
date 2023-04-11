4f5f13161ca8cff0c71fa747aa7cd657ce474f22
JsArafath <arafath.91289@gmail.com>
2023-04-12 00:56:41 +0600

    About implement

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './About.css'
 
 const About = () => {
 
@@ -11,9 +11,11 @@ const About = () => {
       color: '#2A9D8F',
     },
     size: {
-      height: '400px',
-      width: '440px',
-    }
+      width: '500px',
+    },
+    video: {
+      width: '320px',
+    },
   };
 
   return (
@@ -37,19 +39,24 @@ const About = () => {
 <div className=" text-center bg-white sm:p-8 m-5">
 
     <div className="justify-between p-5 space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-          <div className=" sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center  ">
+          <div className=" sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center p-5  ">
             <div className="text-left">
-            <img className="" src="https://i.ibb.co/DMWcZ3y/1-5sp-W5-L8-CWMwm0-P7-Wr-TUXpg-removebg-preview.png" alt="" />
-                
+
+            <div className='relative' >
+            <img  className="pr-16 pb-5 absolute left-0 top-0 size1" src="https://i.ibb.co/hLpd8gk/Rectangle-564.png" alt="" />
+            <img className="pr-20 pb-5 absolute left-0 top-0 size2 pl-1 pb-5" src="https://i.ibb.co/pz1PX8s/Ellipse-1.png" alt="" />
+            </div>
+
+            <img style={styleObject.size} className="" src="https://i.ibb.co/DMWcZ3y/1-5sp-W5-L8-CWMwm0-P7-Wr-TUXpg-removebg-preview.png" alt="" /> 
             </div>
           </div>
 
-        <div className=" sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center p-5 ">
-            <div className="text-left">
+        <div className=" sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center">
+            <div className="text-left p-8">
             <p style={styleObject.textcolor} className="text-gray-900 font-semibold pb-2">Managing Organization</p>
                     <h1  className="text-4xl font-semibold text-gray-700">We Commit To Manage <br></br> Your Organization </h1>
                     <br></br>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">BERTUMBUH - Adalah organisasi non-profit yang mempunyai   <br></br> Melindungi, menjaga, merawat, dan melestarikan pohon-  <br></br> bumi yang kita pijak sekarang. BERTUMBUH - Adalah  <br></br> non-profit yang mempunyai tujuan Melindungi, menjaga, <br></br> dan melestarikan pohon-pohon yang </p>
+                    <p className=" text-base font-normal text-gray-500 dark:text-gray-400">GROW - Is a non-profit organization whose goal is to <br></br> protect, maintain, care for, and preserve the trees on <br></br> the earth that we stand on now</p>
                     <br></br>
                     <button style={styleObject.button} type="button" className="text-white bg-gray-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">   Start A Free Trail</button>
 
