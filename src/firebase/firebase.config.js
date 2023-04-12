9ade0b603da5752c12f004a8be53af176587062f
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-04-12 21:58:12 +0600

    some changes added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBPlVGOHPmdxl1RbM4tF1Hn8a4S2tGUbP8",
-//   authDomain: "organization-manager-cd404.firebaseapp.com",
-//   projectId: "organization-manager-cd404",
-//   storageBucket: "organization-manager-cd404.appspot.com",
-//   messagingSenderId: "190125013833",
-//   appId: "1:190125013833:web:8a9de8bcd06023a94ff898",
-// };
 const firebaseConfig = {
   apiKey: "AIzaSyD9f3gX89D1D61-ioI1KyMHnz4PH4AImqc",
   authDomain: "organization-manager-app.firebaseapp.com",
