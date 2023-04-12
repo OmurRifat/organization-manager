f5b03420d6054f6c91de84e69f1a08723dbf89cf
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-04-12 22:34:59 +0600

    footer issue fixed

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -6,19 +6,17 @@ import Blog from '../Blog/Blog';
 import Newsletter from '../Newsletter/Newsletter';
 import OurPartners from '../OurPartners/OurPartners';
 import FrequentQuestion from '../FrequentQuestion/FrequentQuestion';
-import Footer from '../../Shared/Footer/Footer';
 
 const Home = () => {
     return (
         <div className='mx-2'>
             <HeroSection />
             <About />
-            <Testimonial/>
-            <Blog/>
-            <OurPartners/>
-            <FrequentQuestion/>
-            <Newsletter/>
-            <Footer/>
+            <Testimonial />
+            <Blog />
+            <OurPartners />
+            <FrequentQuestion />
+            <Newsletter />
         </div>
     );
 };
