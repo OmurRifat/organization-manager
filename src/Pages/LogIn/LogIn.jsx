850c16fc434b385b913af5e9cb1433c9e0a9c0ff
JsArafath <arafath.91289@gmail.com>
2023-05-05 20:32:30 +0600

    implemented change password feature and google recaptcha

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,15 +1,17 @@
 import React, { useContext, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import ReCAPTCHA from "react-google-recaptcha";
 import './Login.css'
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import { AuthContext } from '../../context/AuthProvider';
+import ReCAPTCHA from "react-google-recaptcha";
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 import { FcGoogle } from "react-icons/fc"
 import { FaFacebook } from "react-icons/fa"
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
 const LogIn = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm({ mode: "onTouched" });
+  const auth = getAuth();
 
   const styles = {
     bg: {
@@ -17,23 +19,65 @@ const LogIn = () => {
     }
   }
   const { loginUser } = useContext(AuthContext);
-  const navigate = useNavigate();
   function onChange(value) {
     console.log("Captcha value:", value);
   }
+  const navigate = useNavigate();
   const from = location?.state?.from?.pathname || "/";
-  const onSubmit = data => {
-    loginUser(data.email, data.password).then(res => {
-      const user = res.user;
-      if (user.uid) {
-        navigate(from, { replace: true });
-        toast.success("You Have Successfully Signed In!")
-      }
-    }).catch((error) => {
-      const errorMessage = error.message;
-      toast.error(errorMessage)
-    });
+
+  const [userEmail, setUserEmail] = useState("")
+  // const onSubmit = (data, event) => {
+  //   loginUser(data.email, data.password).then(res => {
+  //     const form = event.target;
+  //     const email = form.email.value;
+  //     const password = form.password.value;
+  //     console.log(email, password)
+  //     const user = res.user;
+  //     console.log(user);
+  //     if (user.uid) {
+  //       navigate(from, { replace: true });
+  //       toast.success("You Have Successfully Signed In!")
+  //     }
+  //   }).catch((error) => {
+  //     const errorMessage = error.message;
+  //     toast.error(errorMessage)
+  //   });
+  // };
+  const onSubmit = (data,event) => {
+    event.preventDefault();
+
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password)
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            setSuccess(true);
+        })
+        .catch(error => {
+            console.error('error', error)
+        })
   };
+  const handleonBlur = event => {
+    const email = event .target.value;
+    setUserEmail(email)
+console.log(email)
+  }
+
+  const handleForgetPassword = () => {
+    sendPasswordResetEmail(auth, userEmail)
+  .then(() => {
+    toast.success("Please check your email for verification !!")
+  })
+  .catch((error) => {
+ 
+  });
+
+  }
   return (
     <div>
     {/* grid grid-cols-1 md:grid-cols-2 gap-x-6 */}
@@ -65,17 +109,22 @@ const LogIn = () => {
                     >
                       Email
                     </label>
-                    <input
+                    {/* <input
                       type="email"
-                      id="email"
+                      onBlur={handleonBlur}
+                      name='name'
+                      
                       { ...register("email", {
                         required: "Please Enter Your Email!",
 
                       }) }
+                     
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Email address"
                       required
-                    />
+                    /> */}
+                     <input onBlur={handleonBlur} type="email" name='email' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  placeholder="Email Address"
+               required />
                     <p className='error-message text-red-600'>{ errors.email?.message }</p>
                   </div>
                 </div>
@@ -87,13 +136,13 @@ const LogIn = () => {
                     Password
                   </label>
                   <input
+                  name='password'
                     { ...register("password", { required: "Password is required", minLength: { value: 8, message: "Password must be more than 8 characters" }, maxLength: { value: 12, message: "Password cannot exceed more than 12 characters" } }) }
                     type="password"
                     id="password"
                     placeholder="Enter Password"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
-                    autoComplete='off'
                   />
                   <p className='alerts text-red-600'>{ errors.password?.message }</p>
                 </div>
@@ -103,8 +152,10 @@ const LogIn = () => {
     onChange={onChange}
   />
            </div>
-           {/* <br></br> */}
-                {/* <div className="flex items-start mb-6">
+                <div className='mb-2'>
+                <Link onClick={handleForgetPassword} ><a>Forgot Password ?</a></Link>
+                </div>
+                <div className="flex items-start mb-6">
                   <div className="flex items-center h-5">
                     <input
                       id="remember"
@@ -114,7 +165,7 @@ const LogIn = () => {
                       required
                     />
                   </div>
-
+                 
                   <label
                     htmlFor="remember"
                     className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -122,7 +173,7 @@ const LogIn = () => {
                     I have read and agree with terms of service and our privacy
                     policy
                   </label>
-                </div> */}
+                </div>
                 <button className='bg-[#2A9D8F] text-white p-3 rounded-full text-3xl border-none' type="submit">
                   <AiOutlineArrowRight />
                 </button>
