14e1cd853f0d06ace74dea623dce690f9c332d65
dibbodas12 <dibbodas228@gmail.com>
2023-05-11 14:19:30 +0600

    updated

--- src/Pages/Dashboard/Member/MemberDashboard.jsx
+++ src/Pages/Dashboard/Member/MemberDashboard.jsx
@@ -111,7 +111,7 @@ const MemberDashboard = () => {
             alt=""
           />
           <p className="text-xl text-white py-2">Want to take Loan?</p>
-          { userInfo?.verified === true && userInfo?.position === 'member' && <Link to="/dashboard/apply-loan">
+          { userInfo?.verified === true &&  <Link to="/dashboard/apply-loan">
             <button
               data-modal-target="authentication-modal"
               data-modal-toggle="authentication-modal"
@@ -147,7 +147,7 @@ const MemberDashboard = () => {
             alt=""
           />
           <p className="text-xl text-white py-2">Total Due</p>
-          { userInfo?.verified === true && userInfo?.position === 'member' && <button
+          { userInfo?.verified === true &&<button
             data-modal-target="popup-modal"
             data-modal-toggle="popup-modal"
             onClick={ () => setPayModal(true) }
